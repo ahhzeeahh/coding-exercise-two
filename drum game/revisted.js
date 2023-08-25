037cf441ce61f0f3b113a79a937d1019c354957a
Aziza O'Bryant <140858710+ahhzeeahh@users.noreply.github.com>
2023-08-25 13:41:02 -0500

    Update revisted.js

--- drum game/revisted.js
+++ drum game/revisted.js
@@ -1,10 +1,12 @@
+//--- i thought to change sounds to data from the html to shorten it 1st was naming it after 'i' 0.mp4,1.mp4 ... but then saw class using 'innerHtml' so it was 2 for 1 now with the key press-------
+
 let selectKey = document.querySelectorAll('button');
 let buttons = document.querySelectorAll('button').length;
 
 
 for (let i = 0; i < buttons; ++i) {
     
-    selectKey[i].addEventListener('click', function(e) {
+    selectKey[i].addEventListener('click', function() {
       
       var audio = new Audio('sounds/' + this.innerHTML +'.mp3');
       audio.play();
